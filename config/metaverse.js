4ba4e1132ad36ec26f3c165498e6ab6b1b3fbb5d
Sven <sven@mutzl.org>
2018-06-18 17:22:32 +0200

    Metaverse testnet MIT writer

---
+++ config/metaverse.js
@@ -0,0 +1,5 @@
+module.exports = {
+    use_mit: (process.env.USE_MIT) ? process.env.USE_MIT : 0,
+    mit_content: (process.env.MIT_CONTENT) ? process.env.MIT_CONTENT : '',
+    avatar: (process.env.AVATAR) ? process.env.AVATAR : ''
+};
--- models/metaverse-testnet/index.js
+++ models/metaverse-testnet/index.js
@@ -1,6 +1,9 @@
-var blockchain = require('mvs-blockchain')({url: 'https://explorer-testnet.mvs.org/api/'});
+var blockchain = require('mvs-blockchain')({
+    url: 'https://explorer-testnet.mvs.org/api/'
+});
 let Metaverse = require('metaversejs');
 var config = require('../../config/worker');
+var mvs_config = require('../../config/metaverse');
 
 module.exports = {
     post: post,
@@ -13,7 +16,16 @@ function post(message) {
             blockchain.addresses.txs([wallet.getAddresses()[0]])
             .then(txs => Metaverse.output.calculateUtxo(txs.transactions, [wallet.getAddresses()[0]])) //Get all utxo
             .then((utxos) => Metaverse.output.findUtxo(utxos, {}, 0)) //Collect utxo for given target
-            .then((result) => Metaverse.transaction_builder.send(result.utxo, wallet.getAddresses()[0], {}, wallet.getAddresses()[0], result.change, undefined, (Array.isArray(message) ? message : [message])))
+            .then((result) => {
+                if (mvs_config.use_mit) {
+                    if (mvs_config.avatar) {
+                        return Metaverse.transaction_builder.registerMIT(result.utxo, wallet.getAddresses()[0], mvs_config.avatar, message, mvs_config.mit_content, wallet.getAddresses()[0], result.change);
+                    } else {
+                        throw Error('Incomplete MIT writer configuration');
+                    }
+                }
+                return Metaverse.transaction_builder.send(result.utxo, wallet.getAddresses()[0], {}, wallet.getAddresses()[0], result.change, undefined, (Array.isArray(message) ? message : [message]));
+            })
             .then((tx) => wallet.sign(tx))
             .then((tx) => tx.encode())
             .then((tx) => blockchain.transaction.broadcast(tx.toString('hex')))
